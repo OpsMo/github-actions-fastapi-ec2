0351b7576ef4abfb970c13dc2d8cf2ba9e5becbe
Mo <Mo_Mac>
2025-05-13 21:03:53 +0100

    update main.py

--- app/main.py
+++ app/main.py
@@ -7,8 +7,5 @@ def root():
         return {"version": "1",
                 "status": "running"}
         
-            # return {"message": "FastAPI app workflow deployment Demo",
-    #         "version": "v1",
-    #         "status": "running"}
 
 
