a3bae665f27756d380aa7026d4f7aac5f6fb595d
Mo <Mo_Mac>
2025-05-14 14:29:43 +0100

    add __main__

--- app/main.py
+++ app/main.py
@@ -6,6 +6,8 @@ app = FastAPI()
 def root():
         return {"version": "1",
                 "status": "running"}
-        
 
 
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
