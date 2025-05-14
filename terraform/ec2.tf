c018fc216fc6355eb42928c69c6b24d7f632f811
Mo <Mo_Mac>
2025-05-14 13:11:08 +0100

    Update/ add security group (ssh)

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,8 +1,31 @@
+resource "aws_security_group" "fastapi_ssh_access" {
+  name        = "fastapi-ssh-access"
+  description = "Allow SSH access -port 22"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
 resource "aws_instance" "fastapi_staging" {
   ami                  = var.ec2_ami
   instance_type        = var.instance_type
   key_name             = var.key_name
   iam_instance_profile = aws_iam_instance_profile.ec2_ecr_access.name
+  vpc_security_group_ids = [aws_security_group.fastapi_ssh_access.id]
 
   user_data = <<-EOF
               #!/bin/bash
@@ -23,6 +46,7 @@ resource "aws_instance" "fastapi_prod" {
   instance_type        = var.instance_type
   key_name             = var.key_name
   iam_instance_profile = aws_iam_instance_profile.ec2_ecr_access.name
+  vpc_security_group_ids = [aws_security_group.fastapi_ssh_access.id]
 
   user_data = <<-EOF
               #!/bin/bash
