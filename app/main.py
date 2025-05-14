f688be9664d60aa538d75aa288b10bf8d7757bfd
Mo <Mo_Mac>
2025-05-14 15:04:04 +0100

    block removal

--- app/main.py
+++ app/main.py
@@ -8,6 +8,6 @@ def root():
                 "status": "running"}
 
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000)
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run("main:app", host="0.0.0.0", port=8000)
